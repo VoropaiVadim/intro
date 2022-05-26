eb3e39db57dc31671c3777156606c1fe1ea6b69f
VoropaiVadim <47778283+VoropaiVadim@users.noreply.github.com>
2022-05-26 19:10:58 +0300

    feat: first project in java

---
+++ src/task1.java
@@ -0,0 +1,5 @@
+public class task1 {
+    public static void main(String[] args) {
+                System.out.println("Java: version 11."); //Это мой первый проект Java
+            }
+}
---
+++ src/task2.java
@@ -0,0 +1,10 @@
+public class task2 {
+    public static void main(String[] args) {
+        System.out.println("Java: version 11.");
+        System.out.println("JVM: HotSpot.");
+    }
+}
+
+
+
+
