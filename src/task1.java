c763bf1fc4fe48323f3da0ec94d8b72cc9927ea4
Vadim Voropai <vab18071981@gmail.com>
2022-05-30 15:39:16 +0300

    Update task1.java

--- src/task1.java
+++ src/task1.java
@@ -1,4 +1,4 @@
-public class task1 {
+public class Task1 {
     public static void main(String[] args) {
                 System.out.println("Java: version 11."); //Это мой первый проект Java
             }
